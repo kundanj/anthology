ea66d1499609103d542e66349a0507503359436d
Kundan Joshi <kundan.joshi1@gmail.com>
2023-04-09 13:13:18 +0530

    Updates

---
+++ precache-manifest.0e3c92c8bf4710dc7168d9fd263df0b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f417338cd6a2dc4eb2c",
+    "url": "/anthology/static/css/main.0b6cedad.chunk.css"
+  },
+  {
+    "revision": "1f417338cd6a2dc4eb2c",
+    "url": "/anthology/static/js/main.1f417338.chunk.js"
+  },
+  {
+    "revision": "e8f1cc1eabf30468121e",
+    "url": "/anthology/static/js/1.e8f1cc1e.chunk.js"
+  },
+  {
+    "revision": "287e8311b7626e10de42",
+    "url": "/anthology/static/js/runtime~main.287e8311.js"
+  },
+  {
+    "revision": "ce8934c8d6c29e680fc3647091666b68",
+    "url": "/anthology/index.html"
+  }
+];
